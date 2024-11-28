0b21c3878a7a558e73eef54c2ac4e26d06f54e29
yasirarafath96 <arafathyasir4000@gmail.com>
2024-11-28 23:08:27 +0530

    update program pickers para, surah and pages

--- src/app/updateProgram.js
+++ src/app/updateProgram.js
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import pages from "../../constants/pages.json";
 import rukus from "../../constants/rukus.json";
 import surah from "../..//constants/surah.json";
+import juzzs from "../../constants/juzzs.json";
 import { TextInput } from "react-native-paper";
+import { z } from 'zod';
 
 const UpdateProgram = () => {
   const [selectedPara, setSelectedPara] = useState(1);
   const [page, setPage] = useState(1);
+
+  const [selectedSurah, setSelectedSurah] = useState(null);
+  const [PagesofSelectedSurah, setPagesofSelectedSurah] = useState([]);
 
   const totalPages = pages.length;
   const PageDetail = pages[page - 1];
@@ -23,79 +29,151 @@ const UpdateProgram = () => {
   console.log("page", page);
   console.log("specific page", PageDetail);
 
-  const pageSurah = pages.at(page - 1).surah;
-  const pageStartAyat = pages.at(page - 1).ayah;
+  const currentPage = Number(page);
+
+  const pageSurah = pages.at(page - 1)?.surah;
+  const pageStartAyat = pages.at(page - 1)?.ayah;
 
   console.log("Surah in Page", pageSurah);
   console.log("pageStartAyat", pageStartAyat);
 
-  const surahNumber = surah[pageSurah - 1].number;
-  const surahName = surah[pageSurah - 1].englishName;
-  const surahAyats = surah[pageSurah - 1].numberOfAyahs;  
+  const surahNumber = surah[pageSurah - 1]?.number;
+  const surahName = surah[pageSurah - 1]?.englishName;
+  const surahAyats = surah[pageSurah - 1]?.numberOfAyahs;
 
-  console.log("surahNumber", surahNumber)
-  console.log("surahName", surahName)
-  console.log("Number of Ayats in Surah", surahAyats)
+  console.log("surahNumber", surahNumber);
+  console.log("surahName", surahName);
+  console.log("Number of Ayats in Surah", surahAyats);
 
-  const nextPage = page + 1;
-  const nextPageStartAyat = pages.at(page).ayah;
+  const nextPage = currentPage + 1;
+  const nextPageStartAyat = pages.at(currentPage)?.ayah;
 
-  console.log("nextPage", nextPage)
-  console.log("nextPageStartAyat", nextPageStartAyat)
+  console.log("nextPage", nextPage);
+  console.log("nextPageStartAyat", nextPageStartAyat);
 
-  const numberOfAyatsInPage = (page, pageStartAyat) => {
-    let currentPage = page;
-    let nextPage = currentPage + 1;
-    const numberofPages = nextPage - currentPage;
-  }
+  const totalJuzzs = juzzs.length;
+  const juzzStartSurah = juzzs[0].surah;
+  const juzzStartAyat = juzzs[0].ayah;
 
-  // console.log("selectedPara ",selectedPara)
+  console.log("totalJuzzs", totalJuzzs);
+  console.log("juzzStartSurah , juzzStartAyat", juzzStartSurah, juzzStartAyat);
+  console.log("selectedPara", selectedPara);
+
+  const userSchema = z.object({
+    page: z.number().min(1).max(604, 'page must be from 1 to 604')
+  });
+
+  useEffect(() => {
+    const allPages = Array.from({ length: 604 }, (_, index) => ({
+      pageNumber: index + 1,
+      description: `Page ${index + 1}`,
+    }));
+    setPagesofSelectedSurah(allPages);
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Text>Page: </Text>
-        <TextInput
-          onChangeText={(e) => setPage(e)}
-          value={page}
-          keyboardType="numeric"
-        />
-        <Text>Page {page} surah : {pageSurah}</Text>
-        <Text>Page {page} Ayat Start: {pageStartAyat}</Text>
-        <Text>Page {page}</Text>
+      <ScrollView>
+        <View style={styles.inputContainer}>
+          <Text>Page: </Text>
+          <TextInput
+            onChangeText={(e) => setPage(e)}
+            value={page}
+            keyboardType="numeric"
+          />
+          <Text style={styles.label}>
+            Page {page} surah : {pageSurah}
+          </Text>
+          <Text style={styles.label}>
+            Page {page} Ayat Start: {pageStartAyat}
+          </Text>
+          <Text style={styles.label}>Page {page}</Text>
 
-        <Text>surah Number: {surahNumber}</Text>
-        <Text>Surah Name: {surahName}</Text>
-        <Text>Number of Ayats in Surah: {surahAyats}</Text>
+          <Text style={styles.label}>surah Number: {surahNumber}</Text>
+          <Text style={styles.label}>Surah Name: {surahName}</Text>
+          <Text style={styles.label}>
+            Number of Ayats in Surah: {surahAyats}
+          </Text>
 
-        <Text>Next Page: {nextPage}</Text>
+          <Text style={styles.label}>Next Page: {nextPage}</Text>
 
-        <Text style={styles.label}>Select Para:</Text>
-        <Picker
-          selectedValue={selectedPara}
-          onValueChange={(itemValue) => setSelectedPara(itemValue)}
-          style={styles.picker}
-        >
-          {Array.from({ length: 30 }, (_, index) => (
-            <Picker.Item
-              key={index}
-              label={`Para ${index + 1}`}
-              value={index}
-              color={selectedPara === index ? "#007BFF" : "#000"}
-            />
-          ))}
-        </Picker>
+          <Text style={styles.label}>Select Para: {selectedPara}</Text>
+          <Picker
+            selectedValue={selectedPara}
+            onValueChange={(itemValue) => setSelectedPara(itemValue)}
+            style={styles.picker}
+          >
+            {[...Array(30)].map((_, index) => {
+              const paraValue = index + 1;
+              return (
+                <Picker.Item
+                  key={paraValue}
+                  label={`Para ${paraValue}`}
+                  value={paraValue}
+                  color={selectedPara === paraValue ? "#007BFF" : "#000"}
+                />
+              );
+            })}
+          </Picker>
 
-        {/* Display selected Para */}
-        {selectedPara !== null && (
-          <View style={styles.selectionContainer}>
-            <Text style={styles.selectedText}>
-              Selected Para:{" "}
-              <Text style={styles.bold}>Para {selectedPara + 1}</Text>
-            </Text>
-          </View>
-        )}
-      </View>
+          <Picker
+            selectedValue={selectedSurah}
+            onValueChange={(itemValue) => setSelectedSurah(itemValue)}
+            style={styles.picker}
+          >
+            {surah.map((s, index) => (
+              <Picker.Item
+                key={index}
+                label={`${s.englishName}`}
+                value={s.number}
+              />
+            ))}
+          </Picker>
+
+          <Picker
+            selectedValue={page}
+            onValueChange={(itemValue) => setPage(itemValue)}
+            style={styles.picker}
+          >
+            {PagesofSelectedSurah.map((pageData, index) => (
+              <Picker.Item
+                key={index}
+                label={pageData.description}
+                value={pageData.pageNumber}
+              />
+            ))}
+          </Picker>
+
+          {selectedPara !== null && (
+            <View style={styles.selectionContainer}>
+              <Text style={styles.selectedText}>
+                Selected Para:{" "}
+                <Text style={styles.bold}>Para {selectedPara}</Text>
+              </Text>
+            </View>
+          )}
+
+          {selectedSurah !== null && (
+            <View style={styles.selectionContainer}>
+              <Text style={styles.selectedText}>
+                Selected Surah:{" "}
+                <Text style={styles.bold}>
+                  {surah.find((s) => s.number === selectedSurah)?.englishName ||
+                    "Unknown"}
+                </Text>
+              </Text>
+            </View>
+          )}
+
+          {page !== null && (
+            <View style={styles.selectionContainer}>
+              <Text style={styles.selectedText}>
+                Selected Page: <Text style={styles.bold}>Page {page}</Text>
+              </Text>
+            </View>
+          )}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -115,7 +193,7 @@ const styles = StyleSheet.create({
     elevation: 3,
   },
   label: {
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: "bold",
     marginBottom: 10,
   },
